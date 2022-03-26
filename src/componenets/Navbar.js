daa5293cb62f3af8b17a0b2a1f7bce908f847ebe
garrett h <hurstg02@gmail.com>
2022-03-26 11:13:07 -0400

    setup page

---
+++ src/componenets/Navbar.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Navbar.css'
+import logo from '../images/logo.png'
+
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <img src={logo} />
+        <h1>Gotta Catch Em All</h1>
+    </nav>
+  );
+}
+
+export default Navbar;
